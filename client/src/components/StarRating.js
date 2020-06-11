624728ba8b1a1fb3d9e3e4ec4cde59fa96cd06d0
César Delafargue <cesar.delafargue@student-cs.fr>
2020-06-11 16:31:44 +0200

    Etoiles qui marchent enfin

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -5,37 +5,28 @@ import StarRatings from 'react-star-ratings';
 
  
  
-class Stars extends React.Component {
+function Stars () {
+    
+    const[rating,setRating]=useState(0);
+    {console.log(rating)}    
+     return (
 
-    constructor(props){
-        super(props);
-        this.state={rating:0};
-        this.changeRating = this.changeRating.bind(this);
-        
-    }
+           
+            <StarRatings
+            
+                rating={rating}
+                changeRating={setRating} 
+                starDimension="35px"
+                starSpacing="10px"
+                starHoverColor='gold'
+                starRatedColor='gold'
+                numberOfStars={5}
+            />
+    );
+    
 
-   
-
-    changeRating(rating) {
-        this.setState({
-          rating: rating
-        })
-      }
-
-    render() {
-        // aggregateRating = 2.35;
-        return (
-        <StarRatings
-            rating={this.state.rating}
-            changeRating={this.changeRating}
-            starDimension="40px"
-            starSpacing="15px"
-            starHoverColor='gold'
-            starRatedColor='gold'
-            numberOfStars={5}
-        />
-        );
-    }
 }
 
+
 export default Stars
+
