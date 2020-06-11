1d5a9b8b6bdcbef13a7f0bd2a845432790251d7e
Thomas Bouquet <thomas.bouquet@student-cs.fr>
2020-06-11 17:31:35 +0200

    MyMood opérationnel !!!

--- client/src/components/MyMoodPage.js
+++ client/src/components/MyMoodPage.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from "react";
 import ScrollBar from "./ScrollBar";
 import Mood from "./MyMood";
+import PosterDisplayer from "./Poster";
+import "./MyMoodPage.css";
 
-const MoodScrollBar = () => {
+
+
+function MoodScrollBar() {
+
+        const [genres, setGenres] = useState([{ value: "Sci-Fi", label: "Sci-Fi" },{ value: "Action", label: "Action" },{ value: "Adventure", label: "Adventure" }]);
+
     return (
         <div id="MyMoodScrollBar">
-            {ScrollBar()}
-        </div>
-    )
-}
+            {<ScrollBar handleChange={(e) => {setGenres(e);console.log(genres[0].value,genres[1].value,genres[2].value)}}/>}
+            <div id="MyMoodResults">
+                <p id="IntroPhrase">
+                    Regarding your today mood, we recommand you to watch :
+                </p>
+                <h2>
+                    {Mood(genres[0].value,genres[1].value,genres[2].value)}
+                </h2>
+                {PosterDisplayer(Mood(genres[0].value,genres[1].value,genres[2].value))}
 
-export default MoodScrollBar;
+            </div>
+        </div>
+    );
+
+    
+    };
+    
+
+
+
+export {MoodScrollBar};
